416366f57ae572d0c2c41d00ae02704ffe8d7e36
Muhammad Alfin Aldianto <alvinaldianto@gmail.com>
2021-12-28 09:10:43 +0700

    Tugas 1 ReactJS
    Tugas ini kami kumpulkan untuk NIOMIC

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import MenuUtama from './Page/MenuUtama';
@@ -5,17 +6,19 @@ import MenuProduct from './Page/MenuProduct';
 import MenuKontak from './Page/MenuKontak';
 import MenuTentangKami from './Page/MenuTentangKami';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MenuUtama />
-      <MenuProduct />
-      <MenuKontak />
-      <MenuTentangKami />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <MenuUtama />
+        <MenuProduct />
+        <MenuKontak />
+        <MenuTentangKami />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
